b27da7aae20ef1c942777461d97dbcf2314e1f16
Kuldeep Nageshwar <35416870+kuldeepNageshwar17@users.noreply.github.com>
2018-08-31 14:39:00 +0530

    Add files via upload

---
+++ FileValidation.js
@@ -0,0 +1,11 @@
+(function($){
+	$(".jpgExt").change(function(){
+	alert("filechange");
+	alert(this.files[0].size);
+})
+
+
+	 $(".jpgExt").change(function(){
+	alert("near unati");
+})
+})(window.jQuery);
